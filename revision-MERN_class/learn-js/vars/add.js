2d00abac024429e018b3ab8b7c6894ea1cfd69dc
Muzammil Ibrahim <muzammilbrahim13@gmail.com>
2025-02-04 11:07:37 +0530

    Add revision class notes

---
+++ revision-MERN_class/learn-js/vars/add.js
@@ -0,0 +1,40 @@
+// console.log("Let & Const examples")
+
+// function example(){
+//     let x="hello"
+//     console.log(`${x}, good morning`)
+//     console.log("X used let here, as its block scoped")
+// }
+
+// example()
+
+// // console.log(`${x}, good morning`)
+// const x="hi"
+// console.log(`${x}, good evening`)
+// console.log("X used const here, as its block scoped")
+
+// arithmetic op
+function arith(op1,op2){
+    var add=op1+op2
+    var sub=op1-op2
+    var mul=op1*op2
+    var div=op1/op2
+
+    console.log(add)
+    console.log(sub)
+    console.log(mul)
+    console.log(div)
+}
+
+const a=20
+const b=10
+console.log(arith(a,b))
+
+
+// 1.Declare one variable to store your name,age,and country.log them in a formatted message
+// var name="kevin"
+// var age=23
+// var country="India"
+
+// document.write(`Name is ${name}, age: ${age}, country: ${country}`)
+// console.log(name,age,country)
---
+++ revision-MERN_class/learn-nodejs/index.js
@@ -0,0 +1,1 @@
+console.log("Hello there, we're learning and working with NodeJs today!")
