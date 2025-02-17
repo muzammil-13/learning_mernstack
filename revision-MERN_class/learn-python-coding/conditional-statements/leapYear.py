feb7d1a5a7cac08f4617ebad0e74054b239b4166
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-02-17 22:13:01 +0530

    Add leap year checker and greeting functions in Python

--- revision-MERN_class/learn-python-coding/conditional-statements/leapYear.py
+++ revision-MERN_class/learn-python-coding/conditional-statements/leapYear.py
@@ -3,3 +3,7 @@ task3: check leap year
 '''
 
 year=int(input("Enter the year: "))
+if(year%400==0) or (year%100!=0 and year%4==0):
+    print(f"{year} is a leap year!")
+else:
+    print(f"{year} isn't leap year")
